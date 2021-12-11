bf25ff48102fb2b841c64892a4d4784ce396d7cd
anhtn <tongngocanhcampha@gmail.com>
2021-12-11 22:53:11 +0700

    anhtn fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
   resources :categories, except: [:destroy]
 end
