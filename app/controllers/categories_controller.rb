5016067e597c86a01028a4e449b26696c6875794
anhtn <tongngocanhcampha@gmail.com>
2021-12-11 22:39:24 +0700

    add decentralization(only admin) when create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -46,5 +46,11 @@ class CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(:name)
   end
-
+  
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
 end
